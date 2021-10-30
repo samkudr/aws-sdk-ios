f8faf09857498a62168d5d251dc423b0d6b93dd6
Jithin Roy <royji@amazon.com>
2021-10-30 08:41:52 -0700

    Updated changelog

--- AWSAuthSDK/Tests/AWSMobileClientTests/AWSMobileClientRestartTests.swift
+++
@@ -1,9 +0,0 @@
-//
-//  AWSMobileClientRestartTests.swift
-//  AWSMobileClientTests
-//
-//  Created by Roy, Jithin on 10/30/21.
-//  Copyright © 2021 Amazon Web Services. All rights reserved.
-//
-
-import Foundation
